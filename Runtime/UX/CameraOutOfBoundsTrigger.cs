10efa731ed0eba2d28b526fe8862c4df05a5c88c
Dino Fejzagić <dino.f@live.de>
2023-06-18 19:41:59 +0200

    Add docs

--- Runtime/UX/CameraOutOfBoundsTrigger.cs
+++ Runtime/UX/CameraOutOfBoundsTrigger.cs
@@ -11,6 +11,21 @@ namespace RealityToolkit.CameraService.UX
     [RequireComponent(typeof(Collider))]
     public class CameraOutOfBoundsTrigger : MonoBehaviour
     {
+        [SerializeField, Tooltip("If set, this trigger will raise camera bounds events to the camera service.")]
+        private bool raiseEvents = true;
 
+        /// <summary>
+        /// If set, this trigger will raise camera bounds events to the camera service.
+        /// </summary>
+        /// <remarks>
+        /// You can use this property to programmatically decide when a trigger should be blocking
+        /// or not to the camera, e.g. when the player has unlocked a new area, set this value to <c>false</c>,
+        /// to allow the camera to pass the trigger without being considered out of bounds.
+        /// </remarks>
+        public bool RaiseEvents
+        {
+            get => raiseEvents;
+            set => raiseEvents = value;
+        }
     }
 }
